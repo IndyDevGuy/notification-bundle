7de6523f209d8aa777bf8df593169827cbc3715e
IndyDevGuy <illusiongrouppcs@gmail.com>
2021-04-11 12:03:00 -0400

    -Updated Configuration.php for Symfony 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('mgilet_notification');
-        $rootNode = $treeBuilder->getRoot();
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode->children()
             ->scalarNode('notification_class')
